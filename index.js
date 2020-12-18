12d25a868f94e507aa71ad7704c4dc1654c47e60
Josh Pollock <jpollock412@gmail.com>
2020-12-18 17:24:17 -0500

    build with node maybe

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var shell = require('shelljs');
+ 
+
 (async () => {
-    console.log('Hi Roy')
+    if (!shell.which('git')) {
+        shell.echo('Sorry, this script requires git');
+        shell.exit(1);
+    } else {
+        shell.exec( 'yarn')
+      }
 })()
