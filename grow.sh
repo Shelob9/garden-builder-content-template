cbf2ef835beab17bcbfef6aa95611c9dd0199611
Josh Pollock <jpollock412@gmail.com>
2020-12-18 11:52:38 -0500

    copy env

--- grow.sh
+++ grow.sh
@@ -1,5 +1,7 @@
 # This file builds the digital garden
 
+## clone garden builder
+echo "Cloning from github"
 DIR="digitial-garden-builder"
 if [ -d "$DIR" ]; then
   echo "Already cloned"
@@ -7,5 +9,12 @@ else
   git clone --depth 1 git@github.com:Shelob9/digitial-garden-builder.git
 fi
 
+## Copy env file to client
+FILE=digitial-garden-builder/client/.env
+if test -f "$FILE"; then
+    rm digitial-garden-builder/client/.env
+fi
+
+cp client.env digitial-garden-builder/client/.env
 
 
