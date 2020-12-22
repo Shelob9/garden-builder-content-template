8ee51193d3211b2f7391716461ed78bcb6a11666
Josh Pollock <jpollock412@gmail.com>
2020-12-22 16:32:00 -0500

    break up build script somewhat

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 var shell = require('shelljs');
  
 function git(cmd,errorMessage) {
-        if (shell.exec(cmd).code !== 0) {
-                shell.echo(errorMessage);
-                shell.exit(1);
-            }     
+    if (shell.exec(cmd).code !== 0) {
+        shell.echo(errorMessage);
+        shell.exit(1);
+    } else {
+        return true;
+    }   
+}
+
+function install() {
+    //** Install Garden Builder */
+    if (shell.test('-d', 'digitial-garden-builder')) {
+        shell.rm('-rf', 'digitial-garden-builder');
+    }
+    return git(
+                'git clone --depth 1 git@github.com:Shelob9/digitial-garden-builder.git',
+                'Error: Git checkout failed'
+        )
+}
+
+function html() {
+    /** Create Garden HTML */
+    shell.echo( '!Making HTML and such out of the Garden!')
+    shell.cp('client.env', 'digitial-garden-builder/client/.env');
+    shell.cp('garden.json', 'digitial-garden-builder/client/garden.json');
+    shell.exec('cd digitial-garden-builder/client && yarn')
+    shell.exec('cd digitial-garden-builder/client && yarn build')
+    shell.exec('cd digitial-garden-builder/client && yarn export')
 }
 (async () => {
     /** */
@@ -12,23 +35,10 @@ function git(cmd,errorMessage) {
         shell.echo('Sorry, this script requires git');
         shell.exit(1);
     } else {
-        //** Install Garden Builder */
-        if (shell.test('-d', 'digitial-garden-builder')) {
-            shell.rm('-rf', 'digitial-garden-builder');
-            }
-            git(
-                    'git clone --depth 1 git@github.com:Shelob9/digitial-garden-builder.git',
-                    'Error: Git checkout failed'
-            )
         
+        html();
 
-        /** Create Garden HTML */
-        shell.echo( '!Making HTML and such out of the Garden!')
-        shell.cp('client.env', 'digitial-garden-builder/client/.env');
-        shell.cp('garden.json', 'digitial-garden-builder/client/garden.json');
-        shell.exec('cd digitial-garden-builder/client && yarn')
-        shell.exec('cd digitial-garden-builder/client && yarn build')
-        shell.exec('cd digitial-garden-builder/client && yarn export')
+        
         /** Copy out dir */
         shell.echo( '!Copying to output directory!')
         shell.cp('-R', 'digitial-garden-builder/client/out', 'docs');
